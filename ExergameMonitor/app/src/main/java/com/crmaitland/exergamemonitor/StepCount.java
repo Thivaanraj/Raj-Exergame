048b06c439df14d56af7510e50fcbc8a76822919
Cam eron <Cam eron>
2017-09-24 11:54:26 -0400

    Init

---
+++ ExergameMonitor/app/src/main/java/com/crmaitland/exergamemonitor/StepCount.java
@@ -0,0 +1,13 @@
+package com.crmaitland.exergamemonitor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StepCount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_step_count);
+    }
+}
